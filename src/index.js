9d08187e5eb8dc8b6fb12e3dacbd8e2406fde0e4
OmHawaldar <hawaldarom39@gmail.com>
2025-01-28 23:54:23 +0530

    updated index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 // src/index.js
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'; // Updated import for React 18
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement); // Using createRoot
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
