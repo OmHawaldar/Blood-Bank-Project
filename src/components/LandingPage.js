6a2a0515212f991e2d8fc0afd8a29589965a5635
OmHawaldar <hawaldarom39@gmail.com>
2025-01-30 00:30:15 +0530

    updated LandingPage.js

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,19 +1,16 @@
 // src/components/LandingPage.js
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './LandingPage.css'; // Import CSS specific to LandingPage
 
 function LandingPage() {
   return (
     <div>
       <h1>Welcome to Blood Bank Application</h1>
-      <div>
-        <Link to="/donor">Donors</Link>
-      </div>
-      <div>
-        <Link to="/recipient">Recipients</Link>
-      </div>
-      <div>
-        <Link to="/admin">Admin</Link>
+      <div className="link-container">
+        <Link to="/donor" className="link-button">Donors</Link>
+        <Link to="/recipient" className="link-button">Recipients</Link>
+        <Link to="/admin" className="link-button">Admin</Link>
       </div>
     </div>
   );
